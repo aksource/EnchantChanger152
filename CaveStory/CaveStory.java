1da1a1622e4364364e7061a1573911e4641d5b73
aksource <akira.mew@gmail.com>
2013-08-31 21:39:17 +0900

    CaveStory:Erase BoosterItem.CaveStory and Booster is now separate.

--- CaveStory/CaveStory.java
+++ CaveStory/CaveStory.java
@@ -1,12 +1,7 @@
 package CaveStory;
 
-import net.minecraft.block.Block;
-import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.item.EnumArmorMaterial;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.Configuration;
-import net.minecraftforge.common.MinecraftForge;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.Init;
 import cpw.mods.fml.common.Mod.Instance;
@@ -18,11 +13,10 @@ import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
 import cpw.mods.fml.common.registry.EntityRegistry;
-import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.common.registry.LanguageRegistry;
 
 @Mod(modid="CaveStory", name="CaveStory", version="1.5.2v1")
-@NetworkMod(clientSideRequired=true, serverSideRequired=false, channels={"CaveStory"}, packetHandler=PacketHandler.class)
+@NetworkMod(clientSideRequired=true, serverSideRequired=false/*, channels={"CaveStory"}, packetHandler=PacketHandler.class*/)
 public class CaveStory
 {
 	public static int CaveStoryID;
@@ -51,13 +45,13 @@ public class CaveStory
 
 	public static double movement =1d;
 	public static String TextureDomain = "CaveStory:";
-	public static String Armor08_1 = "/mods/CaveStory/textures/armor/AR08_1.png";
-	public static String Armor08_2 = "/mods/CaveStory/textures/armor/AR08_2.png";
-	public static String Armor20_1 = "/mods/CaveStory/textures/armor/AR20_1.png";
-	public static String Armor20_2 = "/mods/CaveStory/textures/armor/AR20_2.png";
+//	public static String Armor08_1 = "/mods/CaveStory/textures/armor/AR08_1.png";
+//	public static String Armor08_2 = "/mods/CaveStory/textures/armor/AR08_2.png";
+//	public static String Armor20_1 = "/mods/CaveStory/textures/armor/AR20_1.png";
+//	public static String Armor20_2 = "/mods/CaveStory/textures/armor/AR20_2.png";
 	public static String MissileTex ="/mods/CaveStory/textures/items/Missile-rendering.png";
 	public static String SuperMissileTex ="/mods/CaveStory/textures/items/SuperMissile-rendering.png";
-	public static LivingEventHooks livingeventhooks;
+//	public static LivingEventHooks livingeventhooks;
 
 	@Instance("CaveStory")
 	public static CaveStory instance;
@@ -80,52 +74,37 @@ public class CaveStory
 	public void load(FMLInitializationEvent event)
 	{
 		proxy.registerClientInformation();
-		Booster08 = new ItemBooster(CaveStoryID - 256,EnumArmorMaterial.IRON ,2,1).setUnlocalizedName(TextureDomain + "Booster08").setCreativeTab(CreativeTabs.tabCombat);
+//		Booster08 = new ItemBooster(CaveStoryID - 256,EnumArmorMaterial.IRON ,2,1).setUnlocalizedName(TextureDomain + "Booster08").setCreativeTab(CreativeTabs.tabCombat);
 //		Booster08.iconIndex = ModLoader.addOverride("/gui/items.png", "/booster/Booster08.png");
-		LanguageRegistry.addName(Booster08, "Booster0.8");
+//		LanguageRegistry.addName(Booster08, "Booster0.8");
 
-		Booster20 = new ItemBooster(CaveStoryID - 256 + 1,EnumArmorMaterial.DIAMOND ,3,1).setUnlocalizedName(TextureDomain + "Booster20").setCreativeTab(CreativeTabs.tabCombat);
+//		Booster20 = new ItemBooster(CaveStoryID - 256 + 1,EnumArmorMaterial.DIAMOND ,3,1).setUnlocalizedName(TextureDomain + "Booster20").setCreativeTab(CreativeTabs.tabCombat);
 //		Booster20.iconIndex = ModLoader.addOverride("/gui/items.png", "/booster/Booster20.png");
-		LanguageRegistry.addName(Booster20, "Booster2.0");
+//		LanguageRegistry.addName(Booster20, "Booster2.0");
 
 
 		EntityRegistry.registerModEntity(EntityPolarStar.class, "PolarStar", 0, this, 64, 1, true);
 		EntityRegistry.registerModEntity(EntityMissile.class, "Missile", 1, this, 128, 1, true);
 		EntityRegistry.registerModEntity(EntityWeaponOrb.class, "weaponOrb", 2, this, 64, 1, true);
 
-		livingeventhooks = new LivingEventHooks();
-		MinecraftForge.EVENT_BUS.register(livingeventhooks);
+//		livingeventhooks = new LivingEventHooks();
+//		MinecraftForge.EVENT_BUS.register(livingeventhooks);
 
 //		ModLoader.registerKey(this,new KeyBinding("BoosterSwitch",Keyboard.KEY_B), true);
 //		ModLoader.setInGameHook(this, true, true);
 
-		GameRegistry.addRecipe(new ItemStack(Booster08),
-                new Object[]{ "XRX","XPX","X X",
-                Character.valueOf('X'),Item.ingotIron,
-                Character.valueOf('R'),Item.redstoneRepeater,
-                Character.valueOf('P'),Block.pistonBase});
+//		GameRegistry.addRecipe(new ItemStack(Booster08),
+//                new Object[]{ "XRX","XPX","X X",
+//                Character.valueOf('X'),Item.ingotIron,
+//                Character.valueOf('R'),Item.redstoneRepeater,
+//                Character.valueOf('P'),Block.pistonBase});
+//
+//		GameRegistry.addRecipe(new ItemStack(Booster20),
+//                new Object[]{ "I I"," B ","IDI",
+//                Character.valueOf('B'),Booster08,
+//                Character.valueOf('I'),Item.ingotIron,
+//                Character.valueOf('D'),Item.diamond});
 
-		GameRegistry.addRecipe(new ItemStack(Booster20),
-                new Object[]{ "I I"," B ","IDI",
-                Character.valueOf('B'),Booster08,
-                Character.valueOf('I'),Item.ingotIron,
-                Character.valueOf('D'),Item.diamond});
-
-		if(getClass().getPackage() != null)
-		{
-			GameRegistry.addShapelessRecipe(new ItemStack(Booster08,1),
-					new Object[]{
-				new ItemStack(Block.dirt, 1)});
-
-			GameRegistry.addShapelessRecipe(new ItemStack(Booster20,1),
-					new Object[]{
-				new ItemStack(Block.sand, 1)});
-
-
-			GameRegistry.addShapelessRecipe(new ItemStack(Item.plateChain,1),
-					new Object[]{
-				new ItemStack(Block.workbench, 1)});
-		}
 	}
 	@PostInit
 	public void postInit(FMLPostInitializationEvent event)
